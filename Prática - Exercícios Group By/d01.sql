150abaf8a8809557a28e0daef1d52ae47bcff38b
lcfernandez <lcfernandez@cos.ufrj.br>
2022-12-18 23:52:14 -0300

    group by exercises practice fix: d01

--- Prática - Exercícios Group By/d01.sql
+++ Prática - Exercícios Group By/d01.sql
@@ -1,11 +1,12 @@
 SELECT
     s.name AS school,
     c.name AS course,
-    COUNT(e."userId") AS "studentsCount"
+    COUNT(e."userId") AS "studentsCount",
+    e.status
 FROM educations e
 JOIN schools s ON e."schoolId" = s.id
 JOIN courses c ON e."courseId" = c.id
 WHERE e.status = 'ongoing' OR e.status = 'finished'
-GROUP BY s.id, c.id
+GROUP BY s.id, c.id, e.status
 ORDER BY "studentsCount" DESC
 LIMIT 3;
